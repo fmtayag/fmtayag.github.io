5ab89d115c10fd542a876e9b49b6477e7ea0a476
Francis Tayag <zyenapz@gmail.com>
2024-03-04 15:19:44 +0800

    Made the navbar a flexbox

--- ui/src/components/Navbar.js
+++ ui/src/components/Navbar.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return ( 
         <div className="navbar">
-            <Link to="/" className="logo">Francis Tayag</Link>
+            <div className="logo-container">
+                <Link to="/" className="logo">Francis Tayag</Link>
+            </div>
             <div className="links">
                 <Link to="/about">About Me</Link>
                 <Link to="/projects">Projects</Link>
