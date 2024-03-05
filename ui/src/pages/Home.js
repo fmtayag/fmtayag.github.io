3aca5dba3312496c5176b59ac3e930a29e38644f
Francis Tayag <zyenapz@gmail.com>
2024-03-05 09:32:20 +0800

    Add jumbotron

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,11 +1,10 @@
+import Jumbotron from "../components/Jumbotron";
 import ProjectsList from "../components/ProjectsList";
 
 const Home = () => {
     return (
         <div className="home">
-            <h2>Hi, I'm Francis. I'm a freelance software developer</h2>
-            <a href="https://github.com/zyenapz" target="_">GitHub</a>
-            <a href="https://www.linkedin.com/in/francistayag/" target="_">LinkedIn</a>
+            <Jumbotron />
             <ProjectsList />
         </div>
     );
