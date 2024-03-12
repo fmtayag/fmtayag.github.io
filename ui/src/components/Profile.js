89f8394a67116b7dbe35980a0cfff16ceeb3f58f
Francis Tayag <zyenapz@gmail.com>
2024-03-12 17:11:37 +0800

    Replaced socials with 'View Projects' and 'About Me' - Also replaced everything with grayscale for now so I can focus on contrast

--- ui/src/components/Profile.js
+++ ui/src/components/Profile.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../styles/Profile.css';
 
 const Profile = () => {
@@ -7,16 +8,20 @@ const Profile = () => {
                 <h1>Hi, I'm Francis.</h1>
                 <p className="blurb">I'm a full-stack web developer, pixel artist, and writer.</p>
                 <div className="socials">
+                    <Link to="/">My Projects</Link>
+                    <Link to="/">About Me</Link>
+                </div>
+                {/* <div className="socials">
                     <a href="https://github.com/zyenapz" target="_">GitHub</a>
                     <a href="https://www.linkedin.com/in/francistayag/" target="_">LinkedIn</a>
                     <a href="" target="_">Twitter</a>
                     <a href="" target="_">Upwork</a>
-                </div>
+                </div> */}
 
             </div>
 
             <div className="picture">
-                <img src={process.env.PUBLIC_URL + "/images/Project_76-01.jpg"} alt="profile-pic" />
+                <img src={process.env.PUBLIC_URL + "/images/placeholder-1.png"} alt="profile-pic" />
             </div>
         </div>
     );
--- ui/src/components/ProjectsList.js
+++ ui/src/components/ProjectsList.js
@@ -7,10 +7,10 @@ const ProjectsList = () => {
             <h1>My Projects</h1>
 
             <div className="container">
-                <ProjectCard image_src="/images/cat1.png" title="Title 1" subtitle="Subtitle 1" body="Lorem ipsum dolor sit amet tutor carthago delenda" />
-                <ProjectCard image_src="/images/placeholder.png" title="Title 2" subtitle="Subtitle 2" body="Lorem ipsum dolor sit amet tutor carthLorem ipsum dolor sit amet tutor ago delenda" />
-                <ProjectCard image_src="/images/placeholder.png" title="Title 3" subtitle="Subtitle 3" body="Lorem ipsum dolor sit amet tutor carthago delenda Lorem ipsum dolor sit amet tutor carthago delenda Lorem ipsum dolor sit amet tutor carthago delenda" />
-                <ProjectCard image_src="/images/placeholder.png" title="Title 4" subtitle="Subtitle 4" body="Lorem ipsum dolor sit amet tutor carthago delenda " />
+                <ProjectCard image_src="/images/placeholder-1.png" title="Title 1" subtitle="Subtitle 1" body="Lorem ipsum dolor sit amet tutor carthago delenda" />
+                <ProjectCard image_src="/images/placeholder-2.png" title="Title 2" subtitle="Subtitle 2" body="Lorem ipsum dolor sit amet tutor carthLorem ipsum dolor sit amet tutor ago delenda" />
+                <ProjectCard image_src="/images/placeholder-3.png" title="Title 3" subtitle="Subtitle 3" body="Lorem ipsum dolor sit amet tutor carthago delenda Lorem ipsum dolor sit amet tutor carthago delenda Lorem ipsum dolor sit amet tutor carthago delenda" />
+                <ProjectCard image_src="/images/placeholder-2.png" title="Title 4" subtitle="Subtitle 4" body="Lorem ipsum dolor sit amet tutor carthago delenda " />
             </div>
         </div>
     );
