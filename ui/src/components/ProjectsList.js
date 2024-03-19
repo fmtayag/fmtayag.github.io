fa7e321dbdee5f547a34635248e477dcecb0d7bc
Francis Tayag <zyenapz@gmail.com>
2024-03-19 21:38:44 +0800

    Improve UI of 'Project Details' page

--- ui/src/components/ProjectsList.js
+++ ui/src/components/ProjectsList.js
@@ -1,18 +1,56 @@
 import ProjectCard from "./ProjectCard";
 import '../styles/ProjectsList.css';
+import { useState, useEffect } from "react";
 
 const ProjectsList = () => {
+
+    const [data, setData] = useState(null);
+    const url = "http://localhost:8000/projects/"
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => {
+                if (!res.ok) {
+                    throw Error("Could not fetch data");
+                }
+                else {
+                    return res.json();
+                }
+            }
+            )
+            .then(data => {
+                data.forEach((object) => {
+                    console.log(object.id);
+                })
+
+                setData(data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [url])
+
 
     return (
         <div className="projects-list">
             <h1>My Projects</h1>
-
             <div className="container">
-                <ProjectCard id="1" image_src="/images/placeholder-1.png" title="Title 1" subtitle="Subtitle 1" body="Lorem ipsum dolor sit amet tutor carthago delenda" />
-                <ProjectCard id="2" image_src="/images/placeholder-2.png" title="Title 2" subtitle="Subtitle 2" body="Lorem ipsum dolor sit amet tutor carthLorem ipsum dolor sit amet tutor ago delenda" />
-                <ProjectCard id="3" image_src="/images/placeholder-3.png" title="Title 3" subtitle="Subtitle 3" body="Lorem ipsum dolor sit amet tutor carthago delenda Lorem ipsum dolor sit amet tutor carthago delenda Lorem ipsum dolor sit amet tutor carthago delenda" />
-                <ProjectCard id="4" image_src="/images/placeholder-2.png" title="Title 4" subtitle="Subtitle 4" body="Lorem ipsum dolor sit amet tutor carthago delenda " />
+                {data && (
+                    data.map((object) => {
+                        return (
+                            <ProjectCard
+                                id={object.id}
+                                image_src={object.thumbnail}
+                                title={object.title}
+                                subtitle={object.subtitle}
+                                body={object.body}
+                            />
+                        );
+                    })
+                )}
             </div>
+
+
         </div>
     );
 }
--- ui/src/pages/ProjectDetails.js
+++ ui/src/pages/ProjectDetails.js
@@ -1,41 +1,81 @@
-import { useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
 
 const ProjectDetails = () => {
     const [data, setData] = useState(null);
+    const [currentIndex, setCurrentIndex] = useState(1);
+    const initialId = useParams().id;
     const navigate = useNavigate();
 
-    const { id } = useParams();
-    fetch('http://localhost:8000/projects/' + id)
-        .then(res => {
-            if (!res.ok) {
-                navigate("/404");
-                throw Error("Could not fetch data");
-            }
-            else {
-                return res.json();
-            }
+    // This was done to fix the component not re-rendering properly when the browser's back and next buttons are clicked
+    useEffect(() => {
+        if (data != null) {
+            setCurrentIndex(data.findIndex(x => x.id == initialId));
         }
-        )
-        .then(data => {
-            setData(data);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    }, [initialId]);
 
+    useEffect(() => {
+        fetch('http://localhost:8000/projects/')
+            .then(res => {
+                if (!res.ok) {
+                    navigate("/404");
+                    throw Error("Could not fetch data");
+                }
+                else {
+                    return res.json();
+                }
+            }
+            )
+            .then(data => {
+                setData(data);
+                setCurrentIndex(data.findIndex(obj => obj.id == initialId)); // Will return -1 if findIndex fails
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [])
 
-    return (
-        <div className="project-details">
-            {data && (
-                <article>
-                    <h1>{data.title}</h1>
-                    <p>{data.body}</p>
-                </article>
-            )}
+    function handlePrevious() {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+            navigate("/projects/" + data[currentIndex - 1].id);
+        }
+        else {
+            // TODO
+        }
 
-        </div>
-    );
+    }
+    function handleNext() {
+        if (currentIndex < data.length - 1) {
+            setCurrentIndex(currentIndex + 1);
+            navigate("/projects/" + data[currentIndex + 1].id);
+        }
+        else {
+            // TODO
+        }
+    }
+
+    let projectNotFound = currentIndex == -1;
+
+    if (projectNotFound) {
+        navigate("/404");
+    }
+    else {
+        return (
+            <div className="project-details">
+                {data && (
+                    <article>
+                        <h1>{data[currentIndex].title}</h1>
+                        <button onClick={handlePrevious}>Previous</button>
+                        <button onClick={handleNext}>Next</button>
+                        <p>{data[currentIndex].body}</p>
+                    </article>
+                )}
+
+            </div>
+        );
+    }
+
 }
 
 export default ProjectDetails;
