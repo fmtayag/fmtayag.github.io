57afba75597298744a9dfe454c294f182e1556db
Francis Tayag <zyenapz@gmail.com>
2024-04-16 15:11:30 +0800

    Refactored StarFighter.mdx file

--- src/content/config.js
+++ src/content/config.js
@@ -4,10 +4,13 @@ const projectCollection = defineCollection({
     type: 'content',
     schema: z.object({
         title: z.string(),
-        release: z.string(),
+        release: z.coerce.date(),
         summary: z.string(),
         techs: z.array(z.string()),
         roles: z.array(z.string()),
+        pubDate: z.coerce.date(),
+        type: z.string(),
+        links: z.record(z.string(), z.string()),
     })
 })
 
